de7924f14a3c5b1fc8d93345f3d3551bbcdbbc25
Nafitha <nafithajas@gmail.com>
2024-12-06 21:33:13 +0400

    updated email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -4,33 +4,21 @@ import path from 'path';
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Set SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// Function to load and populate the HTML template with dynamic content
 const getTemplate = (templateName, replacements) => {
   try {
-    // Define the path to the views folder where templates are stored
     const templatePath = path.join(__dirname, '../views', `${templateName}.html`);
-
-    // Read the HTML template file
     let template = fs.readFileSync(templatePath, 'utf-8');
-
-    // Replace placeholders with actual dynamic values (from 'replacements' object)
     for (let [key, value] of Object.entries(replacements)) {
       const regex = new RegExp(`{{${key}}}`, 'g');
       template = template.replace(regex, value);
     }
-
     return template;
   } catch (error) {
     console.error('Error reading template:', error);
     throw new Error('Template not found');
   }
 };
-
-// General function to send email with dynamic content
 export const sendEmail = async ({ userEmail, subject, templateName, replacements }) => {
   try {
     // Get the HTML content by loading the appropriate template
@@ -39,13 +27,10 @@ export const sendEmail = async ({ userEmail, subject, templateName, replacements
     const msg = {
       to: userEmail,
       from: process.env.FROM_ADDRESS,  // Your verified SendGrid sender email
-      subject: subject,
-      text:'this is a test mail',
-      // text: replacements.textContent || 'Your order details are provided below.',  // Fallback text content if not provided
-      // html: htmlContent,
-    };
-
-   
+      subject: subject,     
+      text: replacements.textContent,  // Fallback text content if not provided
+      html: htmlContent,
+    }; 
 
     // Send the email via SendGrid
     await sgMail.send(msg);
